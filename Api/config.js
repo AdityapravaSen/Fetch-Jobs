9fbf551655f3d63dc4a0fd36581da191e2c0c003
Adityaprava Sen <adityapravasen0911@gmail.com>
2020-10-31 19:10:57 +0530

    removed config.js from gitignore

---
+++ Api/config.js
@@ -0,0 +1,9 @@
+module.exports={
+    Api_id: process.env.Api_id ,            //Taking the API info as environment variables for security.
+    Api_key: process.env.Api_key,
+    Base_url:'https://api.adzuna.com/v1/api/jobs',
+    Base_keys:'app_id=fa540b98&app_key=fd2b884c89913051c95f73a4888f6c35',
+    Base_args: 'search/1?&results_per_page=12&content-type=application/json',
+
+};
+
